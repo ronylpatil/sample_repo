546ee58d012bf5bc1ddac464a6ff7f350285cc8f
ronilpatil <ronilpatil2000@gmail.com>
2023-12-04 17:15:49 +0530

    version 1

---
+++ demo.py
@@ -0,0 +1,14 @@
+
+
+# creating linked list
+from dataclasses import dataclass
+
+
+class node : 
+     def __init__(self, data, address) -> None :
+          self.data = data
+          self.next = address
+
+
+
+
