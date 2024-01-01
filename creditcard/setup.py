9044fb82aa1f517dfbbce2bedf982472453e6e4a
Ronil Patil <63307564+ronylpatil@users.noreply.github.com>
2024-01-01 11:02:03 +0530

    Add files via upload

---
+++ creditcard/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='experimentation with dvc experimentation',
+    author='ronilpatil',
+    license='',
+)
